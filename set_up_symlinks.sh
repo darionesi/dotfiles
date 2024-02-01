b162c820fddf9c3c8b29f21c04077e4b51617ba6
Dario Nesi <dario.nesi@mail.polimi.it>
2024-02-01 20:07:37 +0000

    setup script

--- set_up_symlinks.sh
+++ set_up_symlinks.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 HERE=`dirname $(realpath $0)`
-ln -s $HERE/config/ ~/.config
-ln -s $HERE/gitconfig ~/.gitconfig
+ln -s -f $HERE/config/ ~/.config
+ln -s -f $HERE/gitconfig ~/.gitconfig
+ln -s -f $HERE/zshrc ~/.zshrc
+ln -s -f $HERE/p10k.zsh ~/.p10k.zsh
